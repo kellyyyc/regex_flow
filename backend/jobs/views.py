bda27e1851873b0ed92341906cc755449a8a5671
kelly <kellyfirstname@gmail.com>
2026-07-04 16:04:05 +1000

    feat: add get_all_jobs route

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .mock_data import MOCK_JOBS
+
+
+def find_job(job_id: int):
+    for job in MOCK_JOBS:
+        if job["id"] == job_id:
+            return job
+
+    return None
+
+
+def serialize_job_status(job):
+    return {
+        "id": job["id"],
+        "file_name": job["file_name"],
+        "status": job["status"],
+        "num_processed": job["num_processed"],
+        "row_count": job["row_count"],
+    }
+
+
+def get_all_jobs(request):
+    jobs = [serialize_job_status(job) for job in MOCK_JOBS]
+    return Response(jobs)
+
+
+@api_view(["GET", "POST"])
+def jobs(request):
+    if request.method == "GET":
+        return get_all_jobs(request)
+
+    if request.method == "POST":
+        pass
